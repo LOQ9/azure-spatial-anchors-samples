3e31b25f3595f9465aedc9c0ed99dae2b44f41c7
Marco Santos <marco.santos@glartek.com>
2019-12-10 16:19:27 +0000

    fix: change authorization method

--- Sharing/SharingServiceSample/Data/SpatialAnchorsTokenService.cs
+++ Sharing/SharingServiceSample/Data/SpatialAnchorsTokenService.cs
@@ -6,6 +6,7 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace SharingService.Data
 {
@@ -15,6 +16,7 @@ namespace SharingService.Data
         // TODO: Update with your own values
         public string spatialAnchorsAccountId = Environment.GetEnvironmentVariable("SPATIAL_ANCHORS_ACCOUNT_ID"); // Account ID from Azure Spatial Anchors account
         public string spatialAnchorsResource = Environment.GetEnvironmentVariable("SPATIAL_ANCHORS_RESOURCE");
+        public string spatialAnchorsPrimaryKey = Environment.GetEnvironmentVariable("SPATIAL_ANCHORS_PRIMARY_KEY");
 
         // AAD configuration
         // TODO: Update with your own values
@@ -32,11 +34,13 @@ namespace SharingService.Data
         public async Task<string> RequestToken()
         {
             // Get the AAD app token
-            var authority = $"https://login.microsoftonline.com/{aadTenantId}";
-            var clientCredential = new ClientCredential(aadClientId, aadClientKey);
-            AuthenticationContext authenticationContext = new AuthenticationContext(authority);
-            AuthenticationResult authenticationResult = await authenticationContext.AcquireTokenAsync(spatialAnchorsResource, clientCredential);
-            string aadAppToken = authenticationResult.AccessToken;
+            //var authority = $"https://login.microsoftonline.com/{aadTenantId}";
+            //var clientCredential = new ClientCredential(aadClientId, aadClientKey);
+            //AuthenticationContext authenticationContext = new AuthenticationContext(authority);
+            //AuthenticationResult authenticationResult = await authenticationContext.AcquireTokenAsync(spatialAnchorsResource, clientCredential);
+            //string aadAppToken = authenticationResult.AccessToken;
+
+            //Console.WriteLine("Task - AAD_APP_TOKEN: " + aadAppToken);
 
             // Use the AAD app token to request a Spatial Anchors token
             using (HttpRequestMessage httpRequest = new HttpRequestMessage())
@@ -44,12 +48,16 @@ namespace SharingService.Data
                 Uri.TryCreate($"https://sts.mixedreality.azure.com/Accounts/{spatialAnchorsAccountId}/token", UriKind.Absolute, out Uri uri);
                 httpRequest.Method = HttpMethod.Get;
                 httpRequest.RequestUri = uri;
-                httpRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", aadAppToken);
+                httpRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", spatialAnchorsAccountId + ":" + spatialAnchorsPrimaryKey);
+
+                Console.WriteLine("Task - Spatial Anchors - HTTP_REQUEST: " + httpRequest);
 
                 using (HttpResponseMessage httpResponse = await this.httpClient.SendAsync(httpRequest))
                 {
                     var responseContent = httpResponse.Content.ReadAsStringAsync().Result;
                     JObject responseJson = JObject.Parse(responseContent);
+
+                    Console.WriteLine("Task - Spatial Anchors - HTTP_RESPONSE: " + responseJson);
 
                     return responseJson["AccessToken"].ToObject<string>();
                 }
